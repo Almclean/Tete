e897c8be7156a2b4017f1fb914fd4b81bdbf0096
Alistair McLean <almclean@Nostromo.local>
2012-09-11 21:16:25 +0100

    Initial Commit

---
+++ src/test/java/Tete/AppTest.java
@@ -0,0 +1,13 @@
+package Tete;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class AppTest {
+
+    @Test
+    public void iAmATest() {
+        fail("Not yet implemented !");
+    }
+}
