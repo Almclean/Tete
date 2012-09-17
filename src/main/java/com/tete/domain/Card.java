67506b3a7c503937d15a89c630efd48c7290f63b
Alistair McLean <al.mclean@gmail.com>
2012-09-17 21:56:55 +0100

    Added hashCode and equals methods for Cards.

--- src/main/java/com/tete/domain/Card.java
+++ src/main/java/com/tete/domain/Card.java
@@ -1,12 +1,17 @@
 package com.tete.domain;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
 public class Card {
 
     private final Value value;
     private final DeckAction action;
+    private final Deck.Suit suit;
 
-    public Card ( final Value value, final DeckAction action ) {
+    public Card(final Deck.Suit suit, final Value value, final DeckAction action) {
         this.value = value;
+        this.suit = suit;
         this.action = action;
     }
 
@@ -16,5 +21,20 @@ public class Card {
 
     public DeckAction getAction() {
         return action;
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(value)
+                .append(action)
+                .append(suit)
+                .toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object rhs) {
+        return new EqualsBuilder()
+                .
     }
 }
